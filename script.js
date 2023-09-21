9ded719f7f902c9bbc653fa002c0e10e95a4ae8e
engrnasir <nasirofficial074@gmail.com>
2023-09-21 19:31:12 +0500

    updated

--- script.js
+++ script.js
@@ -126,13 +126,13 @@ const IncomTax = [
             packages.forEach(el => el.classList.remove('active'))
             if(leaseTerm.value<3){
                 packages[0].classList.add('active')
-                plan1.innerHTML = LEASE_PAYMERNT_PER_MONTH
+                document.getElementById('package-small-price').innerHTML = LEASE_PAYMERNT_PER_MONTH
             }else if(leaseTerm.value>3){
                 packages[2].classList.add('active')
-                plan3.innerHTML = LEASE_PAYMERNT_PER_MONTH
+                document.getElementById('package-large-price').innerHTML = LEASE_PAYMERNT_PER_MONTH
             }else{
                 packages[1].classList.add('active')
-                plan2.innerHTML = LEASE_PAYMERNT_PER_MONTH
+                document.getElementById('package-medium-price').innerHTML = LEASE_PAYMERNT_PER_MONTH
             }
 
 
