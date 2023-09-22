d9b51ae8a1b2aa9d9ae7d55bf789a3ce556f42ea
engrnasir <nasirofficial074@gmail.com>
2023-09-22 15:07:39 +0500

    updated to a5

--- scripta5.js
+++ scripta5.js
@@ -1,5 +1,141 @@
-import vehicles from './vehicles'
-
+const vehicles = [
+    {
+        make:'Tesla',
+        model:'Model 3 RWD',
+        modelName:'Model 3 RW',
+        model:'model-3-rwd',
+        version:'Rear-Wheel Drive',
+        bestMethod:'FBT Exempt Method',
+        driveAwayPrice:67406.00,
+        potentialSaving:1000
+    },
+    {
+        make:'Kia',
+        modelName:'Niro',
+        model:'niro',
+        version:'SG2 GT-Line Single Speed',
+        driveAwayPrice: 78344.81,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:42999
+    },
+    {
+        make:'MG',
+        modelName:'ZS EV',
+        model:'zs-ev',
+        version:'Essenve EV',
+        driveAwayPrice:33990.00,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:25194
+    },
+    {
+        make:'Kia',
+        modelName:'EV 6 GT',
+        model:'ev-6-gt',
+        version:'GT-Line AWD',
+        driveAwayPrice:94293.56,
+        bestMethod:'Statutory Method',
+        potentialSaving:49916 
+    },
+    {
+        make:'Kia',
+        modelName:'EV6 Air',
+        model:'ev6-air',
+        version:'GT-Line AWD',
+        driveAwayPrice:78584,
+        bestMethod:'Statutory Method',
+        potentialSaving:43103 
+    },
+    {
+        make:'Hyundai',
+        modeNamel:'Ioniq 5',
+        model:'ioniq-5',
+        version:'Dynamiq 2WD Ne.V3',
+        driveAwayPrice:77542.50,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:42651 
+    },
+    {
+        make:'Polestar',
+        modelName:'2',
+        model:'2',
+        version:'Long Range Dual Motor',
+        driveAwayPrice:81421.30,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:44334 
+    },
+    {
+        make:'BYD',
+        modelName:'Atto 3',
+        model:'atto-3',
+        version:'Extended',
+        driveAwayPrice:53979.45,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:33107 
+        
+    },
+    {
+        make:'Volvo',
+        modelName:'XC 40 AWD',
+        model:'xc-40-awd',
+        version:'Recharge Pure Electric FWD MY23',
+        driveAwayPrice:76990.00,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:42416 
+    },
+    {
+        make:'Tesla',
+        modelName:'Model 3 - LR',
+        model:'model-3-lr',
+        version:'Long Range',
+        driveAwayPrice:77826.00,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:42850 
+    },
+    {
+        make:'Tesla',
+        modelName:'Model Y',
+        model:'model-y-lr',
+        version:'Long Range',
+        driveAwayPrice:84595.00,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:45886 
+    },
+    {
+        make:'Tesla',
+        modelName:'Model Y',
+        model:'model-y-rw',
+        version:'Rear-Wheel Drive',
+        driveAwayPrice:71049.00,
+        bestMethod:'FBT Exempt Method',
+        potentialSaving:39864 
+    },
+    {
+        make:'Tesla',
+        modelName:'Model Y',
+        model:'model-3-p',
+        version:'Performance',
+        driveAwayPrice:99351.00,
+        bestMethod:'Statutory Method',
+        potentialSaving:0 
+    },
+    
+    
+    
+   
+    
+    
+    {
+        make:'Volvo',
+        modelName:'C40',
+        model:'c-40-awd',
+        version:'Recharge Twin Pure AWD',
+        driveAwayPrice:87342.00,
+        bestMethod:'FBT Exempt Method'
+    },
+    
+   
+    
+]
 const assumptions={financeRate:.0875,commissionRate:.08,regoCost:850,dividingFactor:1.042,stampDutyDenominator:200,stampDutyMultiplier:8.4,luxuryCarTaxThreshold:89332,luxuryCarTax:0,FinancingFees:336,documentFees:900,ClaimableGSTCarValueLimit:68108,ResidualValueRate:.2813,PostTaxDeduction:0,StatutoryMethodRate:.2,DaysInFBTDays:366,MinimumThresholdforFBT:2e3,FBTGrossedUpMultiplier:2.0802,FBTRate:.47,LVA:0,ElectricityCostCentsPerkWH:.3524,ElectricityDefaultValue:.195,MedicareSurcharge:"Not Included",MaintenanceMultiplier:3,MaintenanceFee:.013,EmployerFBTType:"Standard",WhoPaysFBT:"Employee - ECM"};
 
 const IncomTax = [
