2663ec39d39dc41ee68daaa80588f8f8978e237b
engrnasir <nasirofficial074@gmail.com>
2023-09-22 20:11:57 +0500

    updated to e5

--- scripte5.js
+++ scripte5.js
@@ -644,7 +644,7 @@ async function  updateAllValues(){
     }
 
     function gotoForm(){
-        const URL = `/form?firstName=${"oskar"}&lastName=${"rutten"}&phone={"477440329"}&email=${"rutten.oskar@gmail.com"}&employer=${''}&car=${SELECTED_VEHICLE}&businessUse=${BUSINESS_USAGE}&term=${LEASE_PERIOD/12}&income=${OVERALL_INCOME_TAX_SAVING}&carPrice=${''}&fbtMethod=${METHOD}&electricty=${ELECTRICITY_FEE}&service=${MAINTENANCE_FEE}&insurance=${INSURANCE_FEE}&regisration=${REGISTRATION_FEE}&tyres=${TYRES_FEE}&roadside=${ROADSIDE_FEE}&incidentalother=${OTHERS_FEE}&carColour=${''}&quoteCPM=${''}&savingsPm=${OVERALL_INCOME_TAX_SAVING}`
+        const URL = `/form?firstName=${"oskar"}&lastName=${"rutten"}&phone=${"477440329"}&email=${"rutten.oskar@gmail.com"}&employer=${''}&car=${SELECTED_VEHICLE}&businessUse=${BUSINESS_USAGE}&term=${LEASE_PERIOD/12}&income=${OVERALL_INCOME_TAX_SAVING}&carPrice=${''}&fbtMethod=${METHOD}&electricty=${ELECTRICITY_FEE}&service=${MAINTENANCE_FEE}&insurance=${INSURANCE_FEE}&regisration=${REGISTRATION_FEE}&tyres=${TYRES_FEE}&roadside=${ROADSIDE_FEE}&incidentalother=${OTHERS_FEE}&carColour=${''}&quoteCPM=${''}&savingsPm=${OVERALL_INCOME_TAX_SAVING}`
         location.href = URL
     }
 }
