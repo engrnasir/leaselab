e35ab364864032590af3d102428f800bb27f6d58
engrnasir <nasirofficial074@gmail.com>
2023-09-23 04:51:13 +0500

    updated to y1

--- scripty1.js
+++ scripty1.js
@@ -1180,7 +1180,8 @@ async function  updateAllValues(costItem){
     }
     
     function gotoForm(){
-        const URL = `/form?firstName=${"oskar"}&lastName=${"rutten"}&phone=${"477440329"}&email=${"rutten.oskar@gmail.com"}&employer=${''}&car=${SELECTED_VEHICLE}&businessUse=${BUSINESS_USAGE}&term=${LEASE_PERIOD/12}&income=${roundoff(OVERALL_INCOME_TAX_SAVING)}&carPrice=${DRIVE_AWAY_PRICE}&fbtMethod=${METHOD}&electricty=${ELECTRICITY_FEE}&service=${MAINTENANCE_FEE}&insurance=${INSURANCE_FEE}&regisration=${REGISTRATION_FEE}&tyres=${TYRES_FEE}&roadside=${ROADSIDE_FEE}&incidentalother=${OTHERS_FEE}&carColour=${''}&quoteCPW=${roundoff(COST_PER_WEEK)}&savingsPm=${roundoff(OVERALL_INCOME_TAX_SAVING)}&kmPerYear=${KM_PER_YEAR}&totalSavedTax=${roundoff(TOTAL_SAVING_OVERALL)}`
+        //firstName=${"oskar"}&lastName=${"rutten"}&phone=${"477440329"}&email=${"rutten.oskar@gmail.com"}&employer=${''}&
+        const URL = `/form?car=${SELECTED_VEHICLE}&businessUse=${BUSINESS_USAGE}&term=${LEASE_PERIOD/12}&income=${roundoff(OVERALL_INCOME_TAX_SAVING)}&carPrice=${DRIVE_AWAY_PRICE}&fbtMethod=${METHOD}&electricty=${ELECTRICITY_FEE}&service=${MAINTENANCE_FEE}&insurance=${INSURANCE_FEE}&regisration=${REGISTRATION_FEE}&tyres=${TYRES_FEE}&roadside=${ROADSIDE_FEE}&incidentalother=${OTHERS_FEE}&carColour=${''}&quoteCPW=${roundoff(COST_PER_WEEK)}&savingsPm=${roundoff(OVERALL_INCOME_TAX_SAVING)}&kmPerYear=${KM_PER_YEAR}&totalSavedTax=${roundoff(TOTAL_SAVING_OVERALL)}`
         location.href = URL
     }
     initialize();
